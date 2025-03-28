4b51cd76760a000662b9149ca8281e4cfe28b781
Feyza <koc.feyza.fb@gmail.com>
2025-03-28 23:45:27 +0300

    Expenses model, view and controller method are created. Expenses link is added to the navbar in the _Layout file.

---
+++ Models/ExpenseModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpendSmart.Models
+{
+    public class ExpenseModel
+    {
+        public int ExpenseId { get; set; }
+        public decimal Value { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+    }
+}
